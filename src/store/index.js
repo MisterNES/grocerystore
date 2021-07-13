73e325989c8cf15c9878720884332d30c4efd923
tan004 <qq563169939@yahoo.com>
2021-07-13 14:04:45 -0700

    finished display the list

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
 import { combineReducers, applyMiddleware, compose, createStore } from "redux";
+import produceReducer from './produce'
 
 const rootReducer = combineReducers({
-
+    produce: produceReducer,
 });
 
 let enhancer;
 
 if (process.env.NODE_ENV !== "production") {
-    const logger = require("redux-logger").default;
-    const composeEnhancers =
-        window.REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    enhancer = composeEnhancers(applyMiddleware(logger));
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(logger));
 }
 
 const configureStore = (preloadedState) => {
